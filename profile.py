078698100601258428c9200485aad755aa77063d
th786903 <43189005+th786903@users.noreply.github.com>
2018-09-18 09:04:40 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,7 +18,7 @@ for i in range(1, 5):
     node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD" 
     interfaceip = node.addInterface("interface" + str(i)) #source 8.8
     interfaceip.component_id = "eth1" #source 8.6
-    interfaceip.(rspec.IPv4Address("192.168.1.1", "192.168.1.4")) #source 8.6
+    interfaceip.addAddress(rspec.IPv4Address("192.168.1.1", "192.168.1.4")) #source 8.6
     link.addInterface(interfaceip) # source 8.6
     if(i == 1)
         node.routable_control_ip = "true" #source example with use of routable_control_ip
